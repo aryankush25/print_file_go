ce81b1e4e73ab94a128fa7279b2836209300b9c0
Aryan Agarwal <aryankush025@gmail.com>
2024-03-25 21:47:05 +0530

    Program added

--- main.go
+++ main.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
 	fmt.Println(os.Args[1])
+
+	f, err := os.Open(os.Args[1])
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	io.Copy(os.Stdout, f)
+
 }
